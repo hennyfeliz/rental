98af5c36df8b298a794f7bf6531875da9f5ffa0f
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-08-23 19:03:56 -0400

    Inicio proyecto rental backend spring boot

---
+++ back/src/test/java/com/rental/RentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
