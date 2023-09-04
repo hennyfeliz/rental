f88e637d04c7f62e3b20c4c54280d6cd4f65dfc3
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-09-04 09:17:14 -0400

    Borrando footer y cambios de Componente Header y estilos

--- front/src/components/header/Header.jsx
+++ front/src/components/header/Header.jsx
@@ -1,41 +1,20 @@
 
 
 import './Header.css'
-import { Avatar, Link } from 'tdesign-react';
-import electricity from '../../assets/icons/electricity.svg'
-import user from '../../assets/icons/user2.svg'
+import house_icon from '../../assets/icons/house.svg'
 
 const Header = () => {
   return (
     <header>
       <div>
-        <Link theme="default" hover="underline">
-          <img
-            src={electricity}
-            alt='electricity-icon'
-            style={{ width: '42px', height: '42px', marginTop: '10px' }}
-          />
-        </Link>
-      </div>
-      <div className="header-central">
-        <Link theme="default" hover="underline">
-          Products
-        </Link>
-        <Link theme="default" hover="underline">
-          Services
-        </Link>
-        <Link theme="default" hover="underline">
-          Contacts
-        </Link>
-      </div>
-      <div className="header-right">
-        <Link theme="default" hover="underline">
-          <img
-            src={user}
-            alt='electricity-icon'
-            style={{ width: '34px', height: '34px', padding: '8px', }}
-          />
-        </Link>
+        <img
+          alt='house_icon-icon'
+          src={house_icon}
+          className="house_icon"
+        />
+        <h2>
+          Houses
+        </h2>
       </div>
     </header>
   )
