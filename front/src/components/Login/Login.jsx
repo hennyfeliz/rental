85c1d196ddc682f52b17bc76ad8cc76fa6433899
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-09-06 19:40:08 -0400

    Primeros cambios login

--- front/src/components/Login/Login.jsx
+++ front/src/components/Login/Login.jsx
@@ -5,7 +5,10 @@ import './Login.css'
 const Login = () => {
   return (
     <div className="login">
-
+      <span>username</span>
+      <input type='text'/>
+      <span>password</span>
+      <input type='password'/>
     </div>
   )
 }
