cad94e0cc65db86e8d1a58eb2650a34a2ac0ec46
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-09-07 20:51:22 -0400

    Archivo js para fetching de datos autenticacion login

---
+++ front/src/utils/postFetching.js
@@ -0,0 +1,22 @@
+export async function localPostData(username, password) {
+  const url = "http://localhost:8080/publico/authenticate"; // Reemplaza esto con la URL de tu servidor de inicio de sesión
+  const data = {
+    username: username,
+    password: password,
+  };
+
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      return response;
+    })
+    .catch((error) => {
+      console.error("Error al iniciar sesión:", error);
+      throw error;
+    });
+}
