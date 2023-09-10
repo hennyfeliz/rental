b418aa57a3b8338ad44437d92eb6900b1031f1d3
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-09-09 20:40:04 -0400

    Implementando endpoints controladores User y UserRol

--- back/src/main/java/com/rental/controllers/UserController.java
+++ back/src/main/java/com/rental/controllers/UserController.java
@@ -1,12 +1,11 @@
 package com.rental.controllers;
 
+import com.rental.entities.House;
 import com.rental.entities.User;
 import com.rental.service.impl.UserServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +22,31 @@ public class UserController {
     @GetMapping("/")
     public ResponseEntity<List<User>> getAllUsers(){
         return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        return userService.getUserById(id)
+                .map(user -> new ResponseEntity<>(user, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+
+    @PostMapping("/")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        return new ResponseEntity<>(userService.createUser(user), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user){
+        return userService.updateUser(id, user)
+                .map(user1 -> new ResponseEntity<>(user1, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUserById(@PathVariable Long id){
+        return (userService.deleteUser(id)) ? ResponseEntity.ok("User deleted.") : new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
     }
 }
--- back/src/main/java/com/rental/controllers/UserRolController.java
+++ back/src/main/java/com/rental/controllers/UserRolController.java
@@ -1,13 +1,12 @@
 package com.rental.controllers;
 
+import com.rental.entities.House;
 import com.rental.entities.User;
 import com.rental.entities.UserRol;
 import com.rental.service.impl.UserRolServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +24,31 @@ public class UserRolController {
     @GetMapping("/")
     public ResponseEntity<List<UserRol>> getAllUsers(){
         return new ResponseEntity<>(userRolService.getAllUsersRol(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserRol> getUserRolById(@PathVariable Long id){
+        return userRolService.getUserRolById(id)
+                .map(userRol -> new ResponseEntity<>(userRol, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+
+    @PostMapping("/")
+    public ResponseEntity<UserRol> createUserRol(@RequestBody UserRol userRol){
+        return new ResponseEntity<>(userRolService.createUserRol(userRol), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserRol> updateUserRol(@PathVariable Long id, @RequestBody UserRol userRol){
+        return userRolService.updateUserRol(id, userRol)
+                .map(userRol1 -> new ResponseEntity<>(userRol1, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUserRolById(@PathVariable Long id){
+        return (userRolService.deleteUserRol(id)) ? ResponseEntity.ok("UserRol deleted.") : new ResponseEntity<>("UserRol not found", HttpStatus.NOT_FOUND);
     }
 }
