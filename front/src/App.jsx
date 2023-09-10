0a521c674656cf4507c38e207aa6747bdbcf6a7c
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-09-10 11:42:18 -0400

    Cambios componente App y componente Main

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 import Sidebar from './components/Sidebar/Sidebar'
 import Main from './components/Main/Main'
+import Explore from './components/Explore/Explore'
+import Saved from './components/Saved/Saved'
 import Header from './components/Header/Header'
 import { Toaster } from 'sonner'
 import Login from './components/Login/Login'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import Publish from './components/publish/Publish'
 
 function App() {
 
@@ -13,7 +16,7 @@ function App() {
       <BrowserRouter>
         <Toaster />
         <Routes>
-          <Route path='/' element={
+          <Route path='/' exac element={
             <div className="main-container-1">
               <Sidebar />
               <div className="main-container-2">
@@ -21,7 +24,11 @@ function App() {
                 <Main />
               </div>
             </div>
-          } />
+          }>
+            <Route path='/explore' element={<Explore />} />
+            <Route path='/saved' element={<Saved />} />
+            <Route path='/publish' element={<Publish />} />
+          </Route>
           <Route path='/login' element={<Login />} />
         </Routes>
       </BrowserRouter>
--- front/src/components/Main/Main.jsx
+++ front/src/components/Main/Main.jsx
@@ -1,6 +1,7 @@
 
 import Explore from '../Explore/Explore'
 import Saved from '../Saved/Saved'
+import Publish from '../publish/Publish'
 import './Main.css'
 import { Route, Routes } from 'react-router-dom'
 
@@ -9,9 +10,12 @@ const Main = () => {
   return (
     <main>
       <Routes>
-        <Route path='/' element={<Explore />} />
+        <Route path='/' element={<>
+          <h1>Eliga el apartado a buscar...</h1>
+        </>} />
         <Route path='/explore' element={<Explore />} />
         <Route path='/saved' element={<Saved />} />
+        <Route path='/publish' element={<Publish />} />
       </Routes>
     </main>
   )
