382e404c66cd54d95d27d32b9ae2b587eb01313e
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-09-09 20:39:42 -0400

    Implementando endpoints controladores House e Image

--- back/src/main/java/com/rental/controllers/HouseController.java
+++ back/src/main/java/com/rental/controllers/HouseController.java
@@ -4,11 +4,10 @@ import com.rental.entities.House;
 import com.rental.service.impl.HouseServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/houses")
@@ -24,4 +23,32 @@ public class HouseController {
     public ResponseEntity<List<House>> getAllHouses(){
         return new ResponseEntity<>(houseService.getAllHouses(), HttpStatus.OK);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<House> getHouseById(@PathVariable Long id){
+        return houseService.getHouseById(id)
+                .map(house -> new ResponseEntity<>(house, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+
+    @PostMapping("/")
+    public ResponseEntity<House> createHouse(@RequestBody House house){
+        return new ResponseEntity<>(houseService.createHouse(house), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<House> updateHouse(@PathVariable Long id, @RequestBody House house){
+        return houseService.updateHouse(id, house)
+                .map(house1 -> new ResponseEntity<>(house1, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteHouseById(@PathVariable Long id){
+        return (houseService.deleteHouse(id)) ? ResponseEntity.ok("House deleted.") : new ResponseEntity<>("House not found", HttpStatus.NOT_FOUND);
+    }
+
+
 }
--- back/src/main/java/com/rental/controllers/ImageController.java
+++ back/src/main/java/com/rental/controllers/ImageController.java
@@ -1,12 +1,11 @@
 package com.rental.controllers;
 
+import com.rental.entities.House;
 import com.rental.entities.Image;
 import com.rental.service.impl.ImageServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +22,31 @@ public class ImageController {
     @GetMapping("/")
     public ResponseEntity<List<Image>> getAllImages(){
         return new ResponseEntity<>(imageService.getAllImages(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Image> getImageById(@PathVariable Long id){
+        return imageService.getImageById(id)
+                .map(image -> new ResponseEntity<>(image, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+
+    @PostMapping("/")
+    public ResponseEntity<Image> createImage(@RequestBody Image image){
+        return new ResponseEntity<>(imageService.createImage(image), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Image> updateImage(@PathVariable Long id, @RequestBody Image image){
+        return imageService.updateImage(id, image)
+                .map(image1 -> new ResponseEntity<>(image1, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteImageById(@PathVariable Long id){
+        return (imageService.deleteImage(id)) ? ResponseEntity.ok("Image deleted.") : new ResponseEntity<>("Image not found", HttpStatus.NOT_FOUND);
     }
 }
