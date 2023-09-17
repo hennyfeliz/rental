543e78cb7294896d57887bd3499a585ee34ba93f
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-09-16 22:32:06 -0400

    Implementando entidad HouseWithImage para mandar imagenes con House en HouseController

---
+++ back/src/main/java/com/rental/entities/HouseWithImage.java
@@ -0,0 +1,13 @@
+package com.rental.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class HouseWithImage {
+}
