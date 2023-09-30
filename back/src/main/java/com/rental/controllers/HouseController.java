90cbb24d46524710f5877736e2707b8abccd80a4
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-09-29 22:52:32 -0400

    Borrando configuraciones pasadas sobre envio y recivimiendo de imagenes en los controladores de la api

--- back/src/main/java/com/rental/controllers/HouseController.java
+++ back/src/main/java/com/rental/controllers/HouseController.java
@@ -4,7 +4,6 @@ import com.rental.entities.House;
 import com.rental.entities.Image;
 import com.rental.security.rest.SecurityRest;
 import com.rental.service.impl.HouseServiceImpl;
-import com.rental.service.impl.ImageServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.FileSystemResource;
@@ -25,60 +24,18 @@ import java.util.Optional;
 public class HouseController {
 
     private final HouseServiceImpl houseService;
-    private final ImageServiceImpl imageService;
     private static final Logger logger = LoggerFactory.getLogger(SecurityRest.class);
 
 
 
-    public HouseController(HouseServiceImpl houseService, ImageServiceImpl imageService) {
+    public HouseController(HouseServiceImpl houseService) {
         this.houseService = houseService;
-        this.imageService = imageService;
     }
 
     @GetMapping("/")
     public ResponseEntity<List<House>> getAllHouses() throws IOException {
 
         return new ResponseEntity<>(houseService.getAllHouses(), HttpStatus.OK);
-    }
-
-    @GetMapping("/wImg/")
-    public MultiValueMap<String, Object> getImageByIdd(House house) throws IOException {
-        // Lógica para obtener la imagen y otras propiedades
-        Optional<Image> imagee = imageService.getImageById(house.getImage().getId());
-
-        if (imagee == null) {
-            return null;
-        }
-
-        Image image = null;
-        if (imagee.isPresent()) {
-            image = imagee.get();
-        }
-
-        // Cargar la imagen desde el sistema de archivos
-        Path imagePath = Paths.get(image.getFilepath());
-        Resource resource = new FileSystemResource(imagePath.toFile());
-
-        if (!resource.exists()) {
-            return null;
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-
-        // Preparar la respuesta
-        MultiValueMap<String, Object> response = new LinkedMultiValueMap<>();
-        response.add("filename", image.getFilename());
-        response.add("filepath", image.getFilepath());
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.IMAGE_JPEG); // Cambiar según el tipo de imagen
-        headers.setContentDispositionFormData("attachment", image.getFilename());
-        headers.setContentLength(resource.contentLength());
-
-        response.add("image", new HttpEntity<>(resource, headers));
-
-        logger.info("img para response: " + response);
-
-        return response;
     }
 
     @GetMapping("/{id}")
