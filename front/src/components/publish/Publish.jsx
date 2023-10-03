1a01811b442a53df805bbc582bd44676fd827442
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-10-02 23:36:59 -0400

    Componente publish para crear registros de nuevas casas

--- front/src/components/publish/Publish.jsx
+++ front/src/components/publish/Publish.jsx
@@ -2,17 +2,34 @@
 
 import './Publish.css'
 
-
-// ,[address]
-// ,[image]
-// ,[available]
-// ,[owner]
-// ,[rental_info]
-// ,[imagee]
-
 const Publish = () => {
   return (
-    <div className='publish'>Pubasdasdasdlish</div>
+    <div className='publish'>
+      <div>
+        <span>address</span>
+        <input type="text" placeholder='Address' />
+      </div>
+      <div className='available-div'>
+        <span>¿Is the house availabe?</span>
+        <input type='checkbox' className='checkbox-available' />
+      </div>
+      <div>
+        <span>Owner</span>
+        <input type="text" placeholder='Owner' />
+      </div>
+      <div>
+        <span>Rental Info</span>
+        <input type="text" placeholder='Rental Information' />
+      </div>
+      <div>
+        <span>Imagenes</span>
+        <input type="file" placeholder='Rental Information' />
+      </div>
+      <div className='publish-buttons'>
+        <button className='button red'>Limpiar información</button>
+        <button className='button green'>Enviar información de Casa</button>
+      </div>
+    </div>
   )
 }
 
